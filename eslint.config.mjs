0332ef121102123533479923feeca30745c97653
stevenzuluaga <steven.zuluaga@wompi.com>
2025-10-15 17:45:17 -0500

    fix: update ESLint configuration by removing unnecessary Prettier plugin and adjusting recommended settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -9,8 +8,7 @@ export default tseslint.config(
     ignores: ['eslint.config.mjs'],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommendedTypeCheckedOnly,
   {
     languageOptions: {
       globals: {
